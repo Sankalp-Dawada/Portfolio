8c99cf5762f48a119fe1fa57f80da96928764fd1
Sankalp-Dawada <sankalpdawada@gmail.com>
2025-04-19 17:18:01 +0530

    Add show password icon image to the project

--- src/js/login.js
+++ src/js/login.js
@@ -1,11 +1,15 @@
 import { authenticateAdmin } from "./auth.js";
 
+
+// Handle login submission
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   const isAuthenticated = await authenticateAdmin(email, password);
+
   if (isAuthenticated) {
     localStorage.setItem("admin", "true");
     window.location.href = "add-project.html";
@@ -13,3 +17,17 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     alert("Invalid credentials");
   }
 });
+
+  // Toggle password visibility
+  window.togglePassword = function () {
+    const passwordInput = document.getElementById("password");
+    const icon = document.getElementById("eyeIcon");
+  
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      icon.src = "../imgs/showpassword.png";
+    } else {
+      passwordInput.type = "password";
+      icon.src = "../imgs/hidepassword.png";
+    }
+  };
